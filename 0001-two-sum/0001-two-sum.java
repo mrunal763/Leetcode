b3736426cde3a919d2a160fb3d55418e37d3a82c
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-06-29 19:09:43 +0530

    Time: 0 ms (100%), Space: 46.8 MB (83.36%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //int ans[]= new int[2];
+
+        for (int i=1;i<nums.length;i++){
+
+            for(int j=i;j<nums.length;j++)
+            {
+                if(nums[j]+nums[j-i] == target){
+                    return new int[] {j-i,j};
+                }
+            }
+            
+        }
+        return null;
+    }
+}
+  

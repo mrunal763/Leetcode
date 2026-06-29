82f8d11d3a860a192a252ecfcd1bd6fc2e908b49
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-06-29 19:09:12 +0530

    Time: 6 ms (88.74%), Space: 43.7 MB (85.79%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+          int[] m1 = new int[256], m2 = new int[256];
+        
+        // Length of the string
+        int  n = s.length();
+        
+        // Iterate through each character in the strings
+        for (int i = 0; i < n; ++i) {
+            // If the last seen positions of the current characters don't match, return false
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            
+            // Update the last seen positions
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        
+        // If all characters match, return true
+        return true;
+    }
+}

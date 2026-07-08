2584f806d4673961359434efd2cd652098a5931a
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-08 19:47:37 +0530

    Time: 3 ms (93.24%), Space: 44.6 MB (57.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Edge Cases
+        if (s.length() != t.length()) return false;
+
+        // To store the count of each character
+        int[] count = new int[26];
+
+        // Count occurrence of each character in first string 
+        for (char c : s.toCharArray()) count[c - 'a']++;
+
+        // Decrement the count for each character in the second string
+        for (char c : t.toCharArray()) count[c - 'a']--;
+
+        // Check for count of every character
+        for (int i : count) {
+            // If the count is not zero
+            if (i != 0) return false; // Return false
+        }
+
+        // Otherwise strings are anagram
+        return true;
+    }
+}

8b58b95829a10b4886c7c407dfa8e55fc7dc117a
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-21 18:31:27 +0530

    Time: 14 ms (16.45%), Space: 53.4 MB (30.25%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.java
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -1,31 +1,23 @@
 class DiningPhilosophers {
-    Semaphore[] forks = new Semaphore[5];
-    Semaphore eating = new Semaphore(4);
-    public DiningPhilosophers() {
-        for(int i=0;i<5;i++){
-            forks[i] = new Semaphore(1);
-        }
-    }
+    private final ReentrantLock lock = new ReentrantLock();
 
-    // call the run() method of any runnable to execute its code
-    public void wantsToEat(int i,
-                           Runnable pickLeftFork,
-                           Runnable pickRightFork,
-                           Runnable eat,
-                           Runnable putLeftFork,
-                           Runnable putRightFork) throws InterruptedException {
-        eating.acquire();
-        forks[i].acquire();
-        forks[(i+1)%5].acquire();
-        pickLeftFork.run();
-        pickRightFork.run();
-        
-        eat.run();
-        
-        putLeftFork.run();
-        putRightFork.run();
-        forks[i].release();
-        forks[(i+1)%5].release();
-        eating.release();
+    public void wantsToEat(
+            int philosopher,
+            Runnable pickLeftFork,
+            Runnable pickRightFork,
+            Runnable eat,
+            Runnable putLeftFork,
+            Runnable putRightFork) {
+
+        lock.lock();
+        try {
+            pickLeftFork.run();
+            pickRightFork.run();
+            eat.run();
+            putRightFork.run();
+            putLeftFork.run();
+        } finally {
+            lock.unlock();
+        }
     }
 }

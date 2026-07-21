8b277646db34aa1a41cd32ff7b40403c4db504bc
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-21 18:40:14 +0530

    Time: 2 ms (100%), Space: 46.3 MB (78.34%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,11 +1,25 @@
 class Solution {
-
-    public static String intToRoman(int num) {
-        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String ths[]={"","M","MM","MMM"};
+    public String intToRoman(int num) {
+        char[] c = new char[15];
+        int i = 0;
         
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        while (num >= 1000) { c[i++] = 'M'; num -= 1000; }
+        if (num >= 900) { c[i++] = 'C'; c[i++] = 'M'; num -= 900; }
+        if (num >= 500) { c[i++] = 'D'; num -= 500; }
+        if (num >= 400) { c[i++] = 'C'; c[i++] = 'D'; num -= 400; }
+        
+        while (num >= 100) { c[i++] = 'C'; num -= 100; }
+        if (num >= 90) { c[i++] = 'X'; c[i++] = 'C'; num -= 90; }
+        if (num >= 50) { c[i++] = 'L'; num -= 50; }
+        if (num >= 40) { c[i++] = 'X'; c[i++] = 'L'; num -= 40; }
+        
+        while (num >= 10) { c[i++] = 'X'; num -= 10; }
+        if (num >= 9) { c[i++] = 'I'; c[i++] = 'X'; num -= 9; }
+        if (num >= 5) { c[i++] = 'V'; num -= 5; }
+        if (num >= 4) { c[i++] = 'I'; c[i++] = 'V'; num -= 4; }
+        
+        while (num >= 1) { c[i++] = 'I'; num -= 1; }
+        
+        return new String(c, 0, i);
     }
-};
+}

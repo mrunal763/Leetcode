beb62cfdbb30bb09d51ded7b11b710a1d43fdbfb
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-23 19:53:53 +0530

    Time: 46 ms (83.35%), Space: 46.8 MB (8.63%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,22 +1,40 @@
 class Solution {
-    public int repeatedStringMatch(String a, String b) {
-        StringBuilder repeatedA = new StringBuilder(a);
-        int count = 1;
+    public boolean rabinKarp(String text , String pattern){
+        int n = text.length();
+        int m = pattern.length();
 
-        while (repeatedA.length() < b.length()) {
-            repeatedA.append(a);
-            count++;
+        if(n<m) return false;
+        int prime = 101;
+        int base = 256;
+        int hash =1;
+        for(int i=0;i<m-1;i++){
+            hash = (hash*base)%prime;
+        }
+        int pHash=0;
+        int tHash=0;
+        for(int i=0;i<m;i++){
+            pHash = (pHash* base + pattern.charAt(i))%prime;
+            tHash = (tHash*base + text.charAt(i))%prime;
         }
 
-        if (repeatedA.indexOf(b) != -1)
-            return count;
-
-        repeatedA.append(a);
-        count++;
-
-        if (repeatedA.indexOf(b) != -1)
-            return count;
-
+        for(int i=0;i<=n-m;i++){
+            if(pHash==tHash && text.substring(i,i+m).equals(pattern)){
+                return true;
+            }
+            if(i<n-m){
+                tHash = (base*(tHash-hash*text.charAt(i)) + text.charAt(i+m))%prime;
+                if(tHash<0) tHash+=prime;
+            }
+        }
+        return false;
+    }
+    public int repeatedStringMatch(String a, String b) {
+        int max = (int)Math.ceil((double)b.length() / a.length()) + 1;
+        for(int i=1;i<=max;i++){
+            if(rabinKarp(a.repeat(i),b)){
+                return i;
+            }
+        }
         return -1;
     }
 }

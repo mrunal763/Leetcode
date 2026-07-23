58e0a4cbeb7871ca5befe733cfa4254ae5adcb44
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-23 11:43:18 +0530

    Time: 1 ms (51.02%), Space: 125.6 MB (10.2%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,14 +1,6 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-         int n = nums.length;
-
-        if (n <= 2) return n;
-
-        int mask = 0;
-        for (int num : nums) {
-            mask |= num;
-        }
-
-        return mask + 1;
+        int n = nums.length;
+        return n <= 2 ? n : Integer.highestOneBit(n) << 1;
     }
 }

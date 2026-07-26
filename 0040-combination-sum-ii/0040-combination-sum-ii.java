0d2df980724fa57c8669c1044188d7410e5ec472
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-26 19:13:30 +0530

    Time: 4 ms (99.72%), Space: 44.7 MB (96.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,13 +1,14 @@
 class Solution {
 
     private void func(int ind, int sum, List<Integer> nums,
-                      int[] candidates, List<List<Integer>> ans) {
+            int[] candidates, List<List<Integer>> ans) {
         if (sum == 0) {
             ans.add(new ArrayList<>(nums));
             return;
         }
 
-        if (sum < 0 || ind == candidates.length) return;
+        if (sum < 0 || ind == candidates.length)
+            return;
 
         nums.add(candidates[ind]);
 
@@ -23,11 +24,31 @@ class Solution {
         }
     }
 
+    public void quickSort(int from, int to, int[] arr) {
+        if (from >= to)
+            return;
+        int pivot = arr[to];
+        int pt = from;
+        for (int i = from; i < to; i++) {
+            if (arr[i] < pivot) {
+                int tmp = arr[i];
+                arr[i] = arr[pt];
+                arr[pt] = tmp;
+                pt++;
+            }
+        }
+        int tmp = arr[to];
+        arr[to] = arr[pt];
+        arr[pt] = tmp;
+        quickSort(from, pt - 1, arr);
+        quickSort(pt + 1, to, arr);
+    }
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> nums = new ArrayList<>();
 
-        Arrays.sort(candidates);
+        quickSort(0, candidates.length - 1, candidates);
 
         func(0, target, nums, candidates, ans);
         return ans;

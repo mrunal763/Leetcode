1883d9c6d882ed3ff6ace7e193bae9dfeb5585b6
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-07-27 11:38:36 +0530

    Time: 39 ms (74.64%), Space: 54 MB (18.47%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,14 +1,5 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-    
+function createHelloWorld(){
     return function(...args){
         return "Hello World";
     };
-};
-
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
+}

7c1abefa2b1d6360d1ea814a61a32d8ed0f0fe59
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-08-01 11:02:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (89.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,17 @@
 class Solution {
+
     public boolean predictTheWinner(int[] nums) {
-        return sum(nums,0,nums.length-1)>=0;
-    }
-    public int sum(int []nums,int i,int j){
-        if(i==j){
-            return nums[j];
+        
+        int n = nums.length;
+        int[] dp = nums.clone(); 
+        
+        for (int diff = 1; diff < n; ++diff) {
+            for (int j = n - 1; j - diff >= 0; --j) {
+                int i = j - diff;
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
         }
-        int sum1=nums[i]-sum(nums,i+1,j);
-        int sum2=nums[j]-sum(nums,i,j-1);
-        return Math.max(sum1,sum2);
+        
+        return dp[n - 1] >= 0;
     }
 }

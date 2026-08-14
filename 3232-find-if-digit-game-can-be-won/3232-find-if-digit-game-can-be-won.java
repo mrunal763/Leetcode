c7f579c9b69b2b37de098d6afcc5f353af337769
Mrunal Somankar <131909354+mrunal763@users.noreply.github.com>
2026-08-14 13:10:43 +0530

    Time: 1 ms (93.82%), Space: 45.7 MB (8.4%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int count1=0,count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10)count1+=nums[i];
+            else count2+=nums[i];
+        }
+        return count2!=count1;
+    }
+}
